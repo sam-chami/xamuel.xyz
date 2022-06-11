c55a5bc0a6ee77faeec25ac568079b4fa941c1d2
Samuel <samasasamu@protonmail.com>
2022-06-11 23:18:36 +0200

    Add info page, chest and paintings for The Cave

--- thecave.js
+++ thecave.js
@@ -2,14 +2,14 @@ import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/thr
 import { GLTFLoader } from './GLTFLoader.js';
 import { FirstPersonControls } from './FirstPersonControls.js';
 
-let cave, controls;
+let cave, controls, cobject;
 
 const clock = new THREE.Clock();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const lamp = new THREE.PointLight( 0xf51b00, 0.6, 5 );
+const lamp = new THREE.PointLight( 0xf51b00, 0.7, 8 );
 const AmbientLight = new THREE.PointLight( 0xfcd4a9, 0.05, 100 ); 
 lamp.position.set( 10, 1, 2 ); 
 scene.add( lamp );
@@ -19,11 +19,9 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-
 camera.position.z = 5;
-controls = new FirstPersonControls(camera, renderer.domElement)
-controls.lookSpeed = 0.15
+controls = new FirstPersonControls(camera, renderer.domElement);
+controls.lookSpeed = 0.15;
 
 // Instantiate a loader
 const loader = new GLTFLoader();
@@ -36,6 +34,9 @@ loader.load(
 	function ( gltf ) {
 
         cave = gltf.scene;
+
+		cobject = gltf.asset;
+
         cave.position.z = -7;
         cave.position.y = -2.75;
         cave.position.x = -0.45
@@ -65,26 +66,40 @@ loader.load(
 	}
 );
 
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+// Make it posibble to click objects
 
-    controls.update( clock.getDelta() );
-	renderer.render( scene, camera );
+renderer.domElement.addEventListener("click", onclick, true);
 
-    camera.position.y = 0
+const pointer = new THREE.Vector2();
 
-    lamp.position.set(camera.position.x, camera.position.y, camera.position.z - 2)
+var raycaster = new THREE.Raycaster();
 
-    console.log("z:" + camera.position.z)
+function onclick(event) {
+
+	event.preventDefault();
+
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+	raycaster.setFromCamera(pointer, camera);
+
+	console.log("x,y" +pointer.x + pointer.y);
+
+	const intersects = raycaster.intersectObjects(cave.children, false);
+
+	if ( intersects.length > 0 ) {		
+		this.name = intersects[ 0 ].object.name;
+
+		if(this.name === "Chest") {
+			window.location.href = "inf_thecave.html";
+		}
+
+		console.log("name: " + this.name);
+	}
 }
 
+// Resize
 window.addEventListener('resize', onWindowResize, false);
-
-function vh(v) {
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    return (v * h) / 100;
-}
 
 function onWindowResize() {
 
@@ -102,5 +117,19 @@ function onWindowResize() {
 const caveAmbience = new Audio('src/caveAmbience.ogg'); 
 caveAmbience.loop = true;
 caveAmbience.play();
+
+// Animate loop
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    controls.update( clock.getDelta() );
+
+    camera.position.y = 0;
+
+    lamp.position.set(camera.position.x, camera.position.y, camera.position.z);
+	
+	renderer.render( scene, camera );
+}
 
 animate();
